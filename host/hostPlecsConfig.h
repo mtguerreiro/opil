c602cdbcd1174d67cd9b6a4c4ae9b2308971c3ba
Marco Guerreiro <marcotulio.guerreiro@gmail.com>
2023-05-12 19:17:48 +0200

    Added plecs config file

---
+++ host/hostPlecsConfig.h
@@ -0,0 +1,20 @@
+/*
+ * host.h
+ *
+ *  Created on: 12.05.2023
+ *      Author: LRS
+ */
+
+#ifndef HOST_PLECS_CONFIG_H_
+#define HOST_PLECS_CONFIG__H_
+
+//=============================================================================
+/*------------------------------- Definitions -------------------------------*/
+//=============================================================================
+#define HOST_PLECS_CONFIG_MEAS_SIZE				12
+#define HOST_PLECS_CONFIG_SIM_DATA_SIZE			2
+#define HOST_PLECS_CONFIG_CONTROL_SIZE			2
+#define HOST_PLECS_CONFIG_CONTROLLER_SIZE		3
+//=============================================================================
+
+#endif /* HOST_H_ */
